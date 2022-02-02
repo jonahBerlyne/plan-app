ec4e9db6d377d2c6cfd7e9f270303a500c1c1427
jonahBerlyne <jonshaber@gmail.com>
2022-02-02 11:34:47 -0500

    1135am 2/2/22 added project summary

---
+++ src/Components/Projects/ProjectSummary.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function ProjectSummary () {
+ return (
+   <div className='card z-depth-0 project-summary'>
+    <div className='card-content grey-text text-darken-3'>
+     <span className='card-title'>Project Title</span>
+     <p>Posted by JB</p>
+     <p className='grey-text'>2/2/22</p>
+    </div>
+   </div>  
+ );
+}
