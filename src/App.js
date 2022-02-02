92328dc170c0e23cd926eec8a237d1c97a09a5b2
jonahBerlyne <jonshaber@gmail.com>
2022-02-02 11:29:56 -0500

    1130am 2/2/22 added dashboard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './Components/Layout/NavBar';
+import Dashboard from './Components/Dashboard/Dashboard';
 
 export default function App() {
   return (
     <Router>
-    <div className="App">
-      <NavBar/>
-    </div>
+      <div className="App">
+        <NavBar/>
+        <Routes>
+          <Route path="/" element={<Dashboard/>}/>
+        </Routes>
+      </div>
     </Router>
   );
 }
---
+++ src/Components/Projects/ProjectList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default function ProjectList () {
+ return (
+  <div className='project-list section'>
+
+   <div className='card z-depth-0 project-summary'>
+    <div className='card-content grey-text text-darken-3'>
+     <span className='card-title'>Project Title</span>
+     <p>Posted by JB</p>
+     <p className='grey-text'>2/2/22</p>
+    </div>
+   </div>
+
+   <div className='card z-depth-0 project-summary'>
+    <div className='card-content grey-text text-darken-3'>
+     <span className='card-title'>Project Title</span>
+     <p>Posted by JB</p>
+     <p className='grey-text'>2/2/22</p>
+    </div>
+   </div>
+
+   <div className='card z-depth-0 project-summary'>
+    <div className='card-content grey-text text-darken-3'>
+     <span className='card-title'>Project Title</span>
+     <p>Posted by JB</p>
+     <p className='grey-text'>2/2/22</p>
+    </div>
+   </div>
+
+  </div>
+ );
+}
